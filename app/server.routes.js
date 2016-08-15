81a1347cf6a30f74942399891dbba7b2e0da7acd
Harris, Drew <drew.harris@level3.com>
2016-08-15 15:30:41 -0500

    fixed it from not starting by putting the routes in a function

--- app/server.routes.js
+++ app/server.routes.js
@@ -10,15 +10,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var router = express.Router();
 
+module.exports = function (app) {
 
-/************************************************************************************************
- * LOGGING ROUTES
- * @type {exports|module.exports}
- */
-var clientTypes = require('./controllers/clientTypes.server.controller.js');
-router.post('/clienttype/create',clientTypes.createClientType);
-router.post('/clienttype/update',clientTypes.updateClientType);
-router.get('/clienttype/list', clientTypes.listClientType);
-router.get('/clienttype/detail/:typeid', clientTypes.detailClientType);
-router.delete('/clienttype/:typeid', clientTypes.deleteClientType);
+    /************************************************************************************************
+     * LOGGING ROUTES
+     * @type {exports|module.exports}
+     */
+    var clientTypes = require('./controllers/clientTypes.server.controller.js');
+    router.post('/clienttype/create', clientTypes.createClientType);
+    router.post('/clienttype/update', clientTypes.updateClientType);
+    router.get('/clienttype/list', clientTypes.listClientType);
+    router.get('/clienttype/detail/:typeid', clientTypes.detailClientType);
+    router.delete('/clienttype/:typeid', clientTypes.deleteClientType);
 
+};
+//module.exports = router;
