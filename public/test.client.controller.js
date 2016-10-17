eda27ffa4acd2072e012ec3622cedc2f8c37419d
zachharris <the1zachharris@gmail.com>
2016-10-17 17:56:48 -0500

    added function to test files

--- public/test.client.controller.js
+++ public/test.client.controller.js
@@ -52,6 +52,42 @@ test.controller('testController',[
         clientsSearchCalls,
         clientsSettings,
         asvc
-    ){}]);
+    ){
+        /* =====================================================================
+         * Get user get spec'd user from database
+         * ===================================================================== */
+        $scope.getUser = function (user) {
+
+            usersCalls.getUser({
+                username: encodeURIComponent(user.username)
+            }).then(
+                function (res) {
+                    user = angular.copy(res.data);
+
+                    user.rolesByApp = lodash.chain(user.roles)
+                        .groupBy("appName")
+                        .pairs()
+                        .map(function (currentItem) {
+                            return lodash.object(lodash.zip(["name", "roles"], currentItem));
+                        })
+                        .value();
+
+                    // add trackers for role switches in flyout menu in UI
+                    $scope.setRolesForUser(user);
+
+                    // reformat date to readable value
+                    user.created = moment(user.created).format('MM/DD/YYYY HH:MM:SS.SSSS Z');
+                    user.loginTime = moment(user.loginTime).format('MM/DD/YYYY HH:MM:SS.SSSS Z');
+                },
+                function (err) {
+                    console.error('Error getting user details: ' + err.message);
+                    oclLog.log('error', 'Error getting user details: ' + err.message);
+                }
+            );
+        }
+
+    }
+    ]
+        );
 
 
